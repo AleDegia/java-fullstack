2dba0aa81c47499abdff722e2b52bfb8138ac785
AleDegia <degia.ale@gmail.com>
2023-05-15 10:50:52 +0200

    iserita variabile in +

--- es_07/calcolaPerimetroArea.java
+++ es_07/calcolaPerimetroArea.java
@@ -1,5 +1,9 @@
 //package java_workspaces.esercitazioni.es_07;
 
 public class calcolaPerimetroArea {
-    
+
+    public static void main(String[] args) {
+        int lato; //questo sarà il parametro che utilizzerò per i calcoli 
+        lato = 3;
+    }
 }
