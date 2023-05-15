b695c2ba2ab7ef7309d689ef8aa1a4bb857de6f7
AleDegia <degia.ale@gmail.com>
2023-05-15 11:50:22 +0200

    05 inserito prompt

--- es_07/calcolaPerimetroArea.java
+++ es_07/calcolaPerimetroArea.java
@@ -1,14 +1,19 @@
 //package java_workspaces.esercitazioni.es_07;
 
+import java.util.Scanner;
+
 public class calcolaPerimetroArea {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("inserisci la lunghezza del lato");
         int lato; //questo sarà il parametro che utilizzerò per i calcoli 
-        lato=5;
+        lato= input.nextInt();
+        //lato=5;
         int area= lato+lato;
         int perimetro= lato*4;
         System.out.println(lato);
-        System.out.print(area);
+        System.out.println(area);
         System.out.println(perimetro);
     }
 }
