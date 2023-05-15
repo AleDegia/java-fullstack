54413b6d75bb13a92de4da57945fb610faf42245
AleDegia <degia.ale@gmail.com>
2023-05-15 11:37:30 +0200

    04 calcoli e differenze tra print e println

--- es_07/calcolaPerimetroArea.java
+++ es_07/calcolaPerimetroArea.java
@@ -4,6 +4,11 @@ public class calcolaPerimetroArea {
 
     public static void main(String[] args) {
         int lato; //questo sarà il parametro che utilizzerò per i calcoli 
-        lato = 3;
+        lato=5;
+        int area= lato+lato;
+        int perimetro= lato*4;
+        System.out.println(lato);
+        System.out.print(area);
+        System.out.println(perimetro);
     }
 }
