380ff4995c9f86d8a4b8cccfa6197feb96b8a5d7
AleDegia <degia.ale@gmail.com>
2023-05-15 12:58:49 +0200

    prova

--- es_07/calcolaPerimetroArea.java
+++ es_07/calcolaPerimetroArea.java
@@ -15,17 +15,13 @@ public class calcolaPerimetroArea {
         // aggiunto una condizione che verifica l'input
         //aggiungo una condizione aggiuntiva che verifica che l'input sia tra 2 valori specifici
         if (lato > 40 || lato < 1) {
-<<<<<<< HEAD
-            if (lato>40){
+            if (lato>50){
                 System.out.println("hai inserito un numero grande");
             }
             System.out.println("hai inserito un numero non valido");
             if(lato<1){
                 System.out.println("Hai inserito un numero piccolo");
             }
-=======
-            System.out.println("hai inserito un numero non valido");
->>>>>>> 87fe5653ce2e9a7115377c1e5065f151b7f0937a
 
         } else {
             //se l'input è <= a 40
