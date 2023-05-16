6578776225b39a28f47536de80b6452d9da87f34
AleDegia <degia.ale@gmail.com>
2023-05-16 15:18:41 +0200

    ver 1

---
+++ es_10/ArrayForEach.java
@@ -0,0 +1,11 @@
+//package java_workspaces.esercitazioni.es_10;
+
+public class ArrayForEach {
+    public static void main(String[] args) {
+        // String[] array = new String[3];
+        String[] array = { "a", "b", "c", "d", "e" };
+        for (String lettera : array) {
+            System.out.println(lettera);
+        }
+    }
+}
